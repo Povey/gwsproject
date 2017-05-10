608a3b699d2475de155ca2d48830dc06afc97788
Daniel <danielpovey2@hotmail.co.uk>
2017-05-10 04:58:16 +0100

    intital

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ lock "3.8.1"
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
 
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
