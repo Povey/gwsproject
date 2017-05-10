36042efcd1d9b66700d44869894c13b3cc88b8c5
Daniel <danielpovey2@hotmail.co.uk>
2017-05-10 05:01:04 +0100

    intital

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -21,7 +21,7 @@
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
-config.serve_static_assets = true
+
 
 # Configuration
 # =============
