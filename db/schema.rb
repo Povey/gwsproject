4d4c214a440fd4f260fd51f9319c6fd733748f9a
Daniel <danielpovey2@hotmail.co.uk>
2017-05-11 09:39:17 +0100

    Final Commit

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,6 @@ ActiveRecord::Schema.define(version: 20170511081803) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "category_id"
-    t.string   "attachment_file_name"
-    t.string   "attachment_content_type"
-    t.integer  "attachment_file_size"
-    t.datetime "attachment_updated_at"
   end
 
   create_table "samples", force: :cascade do |t|
